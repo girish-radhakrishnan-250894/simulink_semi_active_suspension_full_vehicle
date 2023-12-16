1b6dccaf26b85e84771d4987b74166a50c275d54
girish-radhakrishnan-250894 <girish.radhakrishnan@optimumg.com>
2023-12-16 10:47:43 -0700

    Updates

--- signal_processing.m
+++ signal_processing.m
@@ -2,11 +2,22 @@
 
 zs_ddot = out.acc.Data;
 
+suspension_deflection_1 = out.suspension_deflection_1.Data;
+
+damper_velocity_1 = out.damper_velocity_1.Data;
+
+F_t_1 = out.F_t_1.Data;
+
 t = out.t.Data;
+
+rms_zs_ddot = rms(zs_ddot)
+rms_F_t_1 = rms(F_t_1)
+rms_suspension_deflection_1 = rms(suspension_deflection_1)
+rms_damper_velocity_1 = rms(damper_velocity_1)
 
 %% Signal Processing Parameters
 
-fres = 0.2; % Required Frequency Resolution
+fres = 0.1; % Required Frequency Resolution
 
 fs = 1/0.001; % Samping frequency
 
@@ -25,7 +36,7 @@ loglog(hz_zs_ddot,psd_zs_ddot)
 hz_weight_ride_comfort_raw = ([0.1  1    4  8  80 100 200 300 400 500]);
 weight_ride_comfort_raw =    ([0.5  0.5  1  1  0.1 0.1 0.1 0.1 0.1 0.1]);
 
-weight_ride_comfort_filt = interp1(hz_weight_ride_comfort_raw, weight_ride_comfort_raw, (hz_zs_ddot),"linear");
+weight_ride_comfort_filt = interp1(hz_weight_ride_comfort_raw, weight_ride_comfort_raw, (hz_zs_ddot),"linear","extrap");
 
 figure
 loglog((hz_weight_ride_comfort_raw), (weight_ride_comfort_raw))
@@ -38,5 +49,6 @@ ride_comfort_index = psd_zs_ddot .* (weight_ride_comfort_filt);
 figure
 loglog(hz_zs_ddot,ride_comfort_index)
 
+cumtrapz(ride_comfort_index)
 
 
